368752aa24883395e13de906905f3821ca4c1d58
Shuyan Liu <shuyan.liu@and.digital>
2022-09-06 23:44:10 +0100

    2.1 amended

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -11,12 +11,10 @@ const Header = ({course}) => {
   return <h1>{course}</h1>
 }
 
-const Content = (props) => {
+const Content = ({parts}) => {
   return (
     <>
-      <Part part={props.parts[0]} />
-      <Part part={props.parts[1]} />
-      <Part part={props.parts[2]} />
+      {parts.map(part => <Part key={part.id} part={part} />)}
     </>
   )
 }
