a101cb126b24c71cbb26616546e052bd98d148b1
clemcodes <clemcodes@outlook.com>
2022-09-09 16:32:14 +0100

    2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,34 +1,31 @@
 import { useState } from 'react'
 
-const Person = ({person}) => {
+const Person = ({ele}) => {
   return (
     <>
-      <div>{person.name} {person.phone}</div>
+      <div>{ele.name} {ele.number}</div>
     </>
   )
 }
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phone: '040-1234567'}
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNum, setNewNum] = useState('')
-
-  const handleNoteChange = (e) => {
-    setNewName(e.target.value)
-  }
-
-  const handleNumChange = (e) => {
-    setNewNum(e.target.value)
-  }
+  const [search, setSearch] = useState('')
   
+  const searchResults = persons.filter(person => person.name.toLowerCase().includes(search.toLowerCase()))
   
   const addPerson = (e) => {
     e.preventDefault()
     const newPerson = {
       name: newName,
-      phone: newNum
+      number: newNum
     }
     const isNew = JSON.stringify(persons).indexOf(JSON.stringify(newPerson)) < 0
     if(isNew){
@@ -46,12 +43,14 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with <input type="search" value={search} onChange={(e) => setSearch(e.target.value)}/></div>
+      <h2>Add a new</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={handleNoteChange} />
+          name: <input value={newName} onChange={(e) => setNewName(e.target.value)} />
         </div>
         <div>
-          number: <input value={newNum} onChange={handleNumChange} />
+          number: <input value={newNum} onChange={(e) => setNewNum(e.target.value)} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -59,7 +58,9 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
         <div>
-          {persons.map(person => <Person key={person.name} person={person}/>)}
+          {search 
+            ? searchResults.map(ele => <Person key={ele.name} ele={ele}/>) 
+            : persons.map(ele => <Person key={ele.name} ele={ele}/>)}
         </div>
     </div>
   )
