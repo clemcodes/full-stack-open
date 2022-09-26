d35fcba09ba6e46849d49f89854768f4aebf2b42
clemcodes <clemcodes@outlook.com>
2022-09-26 17:06:20 +0100

    3.2 GET /info

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -28,6 +28,14 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+app.get('/info', (req, res) => {
+    res.send(
+        `<p>Phone book has infor for ${persons.length}</p>
+         <p>${new Date()}</p>
+        `
+        )
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
