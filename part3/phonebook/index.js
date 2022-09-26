ade3e7cc4fd1fcec32b3968697a0ec1d3f10397f
clemcodes <clemcodes@outlook.com>
2022-09-26 17:24:50 +0100

    3.3 fetching a single resource

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -28,6 +28,17 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if(person){
+        res.json(person)
+    } else {
+        res.sendStatus(404)
+    }
+})
+
 app.get('/info', (req, res) => {
     res.send(
         `<p>Phone book has infor for ${persons.length}</p>
