e616517592244bd001d1d80c721bdfe56f062199
clemcodes <clemcodes@outlook.com>
2022-10-03 13:03:50 +0100

    3.9+3.10+3.11 integration and deployment

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,10 +1,13 @@
 const express = require('express')
-const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
+const app = express()
 
 app.use(express.json())
 app.use(morgan('tiny'))
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+app.use(cors())
+app.use(express.static('build'))
 
 let persons = [
     { 
@@ -88,7 +91,7 @@ app.get('/info', (req, res) => {
         )
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
