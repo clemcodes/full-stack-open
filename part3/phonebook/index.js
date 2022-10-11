6cfca1baca59ba51d266d07d4f683aa11b81ddce
clemcodes <clemcodes@outlook.com>
2022-10-11 14:23:21 +0100

    3.14 | creating entry integrated with db

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -60,19 +60,11 @@ app.post('/api/persons', morgan(':body'), (req, res) => {
         })
       }
 
-    const notUnique = persons.find(person => person.name === body.name)
-    if(notUnique){
-        return res.status(400).json({ 
-            error: 'name must be unique' 
-          })
-    }
-
-    const person = {
-        id: Math.floor(Math.random()*10000),
+    const person = new Person({
         name:body.name,
         number:body.number,
-    }
-    persons = persons.concat(person)
+    })
+    person.save()
     res.json(person)
 })
 
