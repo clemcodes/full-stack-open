3a9759195bd0ea9ed1bfd48fab226baac5ac9b46
clemcodes <clemcodes@outlook.com>
2022-10-12 10:39:14 +0100

    3.15 | deleting entry integrated with db

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -7,83 +7,62 @@ const Person = require('./models/person')
 
 app.use(express.json())
 app.use(morgan('tiny'))
-morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+morgan.token('body', function (req, res) {
+  return JSON.stringify(req.body)
+})
 app.use(cors())
 app.use(express.static('build'))
 
-// let persons = [
-//     { 
-//       "id": 1,
-//       "name": "Arto Hellas", 
-//       "number": "040-123456"
-//     },
-//     { 
-//       "id": 2,
-//       "name": "Ada Lovelace", 
-//       "number": "39-44-5323523"
-//     },
-//     { 
-//       "id": 3,
-//       "name": "Dan Abramov", 
-//       "number": "12-43-234345"
-//     },
-//     { 
-//       "id": 4,
-//       "name": "Mary Poppendieck", 
-//       "number": "39-23-6423122"
-//     }
-// ]
-
 app.get('/api/persons', (req, res) => {
-  Person.find({}).then(persons => {
+  Person.find({}).then((persons) => {
     res.json(persons)
-  })   
+  })
 })
 
 app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(person => person.id === id)
+  const id = Number(req.params.id)
+  const person = persons.find((person) => person.id === id)
 
-    if(person){
-        res.json(person)
-    } else {
-        res.sendStatus(404)
-    }
+  if (person) {
+    res.json(person)
+  } else {
+    res.sendStatus(404)
+  }
 })
 
 app.post('/api/persons', morgan(':body'), (req, res) => {
-    const body = req.body
+  const body = req.body
 
-    if (!body.name || !body.number) {
-        return res.status(400).json({ 
-          error: 'The name or number is missing' 
-        })
-      }
-
-    const person = new Person({
-        name:body.name,
-        number:body.number,
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: 'The name or number is missing',
     })
-    person.save()
-    res.json(person)
+  }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+  person.save()
+  res.json(person)
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    res.status(204).end()
+  Person.findByIdAndRemove(req.params.id)
+    .then(result => {
+        res.status(204).end()
+  })
 })
 
 app.get('/info', (req, res) => {
-    res.send(
-        `<p>Phone book has infor for ${persons.length}</p>
+  res.send(
+    `<p>Phone book has infor for ${persons.length}</p>
          <p>${new Date()}</p>
         `
-        )
+  )
 })
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
