125c4390b6f41cfce93c1c0ab0aa1fafa6ee43bd
clemcodes <clemcodes@outlook.com>
2023-05-06 16:58:11 +0100

    4.22 | userExtractor middleware

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,7 +1,6 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
-const User = require('../models/user')
-const jwt = require('jsonwebtoken')
+const { userExtractor } = require('../utils/middleware')
 
 blogsRouter.get('/', async (req, res) => {
   const blogs = await Blog.find({}).populate('user',{username: 1, name: 1})
@@ -9,18 +8,10 @@ blogsRouter.get('/', async (req, res) => {
   res.json(blogs)
 })
 
-blogsRouter.post('/', async (req, res, next) => {
-  const body = req.body
+blogsRouter.post('/', userExtractor, async (req, res, next) => {
+  const body =req.body
 
-  const decodedToken = jwt.verify(req.token, process.env.SECRET)
-
-  if(!decodedToken.id){
-    return res.status(401).json({ error: 'Token invalid' })
-  }
-
-  const user = await User.findById(decodedToken.id)
-
-  if(!req.body.title || !req.body.url) {
+  if(!body.title || !body.url) {
     res.status(400).send({ error: 'bad request' })
   } else {
     const blog = new Blog({
@@ -28,15 +19,15 @@ blogsRouter.post('/', async (req, res, next) => {
       author: body.author,
       url: body.url,
       likes: body.likes || 0,
-      user: user._id
+      user: req.user.id
     })
 
     try {
       const savedBlog = await blog.save()
       res.status(201).json(savedBlog)
 
-      user.blogs = user.blogs.concat(savedBlog._id)
-      await user.save()
+      req.user.blogs = req.user.blogs.concat(savedBlog._id)
+      await req.user.save()
     } catch (exception) {
       next(exception)
     }
@@ -45,15 +36,11 @@ blogsRouter.post('/', async (req, res, next) => {
   }
 })
 
-blogsRouter.delete('/:id', async (req, res) => {
-  const decodedToken = jwt.verify(req.token, process.env.SECRET)
-  if(!decodedToken.id){
-    return res.status(401).json({ error: 'Token invalid' })
-  }
-  
+blogsRouter.delete('/:id', userExtractor, async (req, res) => {
+
   const blog = await Blog.findById(req.params.id)
 
-  if(blog.user.toString() !== decodedToken.id.toString()){
+  if(blog.user.toString() !== req.user.id.toString()){
     res.status(401).json({ error: 'unauthorized operation' })
   } 
 
--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -1,4 +1,6 @@
 const logger = require('./logger')
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
 
 const requestLogger = (req, res, next) => {
     logger.info('Method: ', req.method)
@@ -14,7 +16,19 @@ const tokenExtractor = (req, res, next) => {
         req.token = authorization.substring(7)
     } 
     next()
-  }
+}
+
+const userExtractor = async (req, res, next) => {
+    if(req.token) {
+        const decodedToken = jwt.verify(req.token, process.env.SECRET)
+        if(!decodedToken.id){
+            return res.status(401).json({ error: 'Token invalid' })
+        }
+        req.user = await User.findById(decodedToken.id)
+    }
+    
+    next()
+}
 
 const unknownEndpoint = (req, res) => {
     res.status(404).send({ error: 'unknown endpoint' })
@@ -37,6 +51,7 @@ const errorHandler = (error, req, res, next) => {
 module.exports = {
     requestLogger,
     tokenExtractor,
+    userExtractor,
     unknownEndpoint,
     errorHandler
   }
