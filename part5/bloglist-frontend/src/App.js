040880adcf3c60a9b100f80eb6f69e209c3dedd1
clemcodes <clemcodes@outlook.com>
2023-05-11 17:35:12 +0100

    5.2 | save to localStorage & logout functionality

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -8,6 +8,7 @@ const App = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -15,15 +16,45 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUser = window.localStorage.getItem('loggedUser')
+    if(loggedUser) {
+      const userJSON = JSON.parse(loggedUser)
+      setUser(userJSON)
+      blogService.setToken(userJSON.token)
+    }
+  }, [])
+
   const handleSubmit = async (e) => {
     e.preventDefault()
-    const userInDb = await loginService.login({username, password})
-    setUser(userInDb)
+    try {
+      const userInDb = await loginService.login({username, password})
+      window.localStorage.setItem('loggedUser', JSON.stringify(userInDb))
+      blogService.setToken(userInDb.token)
+      setUser(userInDb)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setUsername('')
+      setPassword('')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  const handleLogout = (e) => {
+    e.preventDefault()
+    window.localStorage.removeItem('loggedUser')
+    setUser(null)
+    blogService.setToken(null)
   }
 
   const loginForm = () => {
     return (
       <form onSubmit={handleSubmit}>
+        {errorMessage}
         <h1>Log in to application</h1>
         <div><label>username: <input type="text" name="username" value={username} onChange={(e) => setUsername(e.target.value)} /></label></div>
         <div><label>password: <input  type="text" name="password" value={password} onChange={(e) => setPassword(e.target.value)} /></label></div>
@@ -38,6 +69,7 @@ const App = () => {
         {!user && loginForm()}
         <h2>blogs</h2>
         <p>{user.name} logged in</p>
+        <button onClick={handleLogout}>logout</button>
         {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
         )}
