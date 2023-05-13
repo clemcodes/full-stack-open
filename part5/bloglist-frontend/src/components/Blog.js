401b32b85c6afa5034daee0f1790e68296c086de
clemcodes <clemcodes@outlook.com>
2023-05-13 21:29:54 +0100

    5.6 - 5.7 | toggle blog detail

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,27 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author} {blog.url}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({blog}) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [visible, setVisible] = useState(false)
+
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title}
+      <button onClick={() => setVisible(!visible)}>{visible ? 'hide' : 'view'}</button>
+      <div style={ showWhenVisible }>
+      {blog.author} {blog.url} {blog.likes}
+      </div>   
+    </div>   
+  )
+}
 
 export default Blog
