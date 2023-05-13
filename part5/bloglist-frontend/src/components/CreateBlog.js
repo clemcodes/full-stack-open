fbb4ce09f18673337deef6161a7fb710f5bdda89
clemcodes <clemcodes@outlook.com>
2023-05-13 18:10:09 +0100

    5.5 | display blog form when needed

--- part5/bloglist-frontend/src/components/CreateBlog.js
+++ part5/bloglist-frontend/src/components/CreateBlog.js
@@ -4,26 +4,42 @@ export const CreateBlog = (props) => {
   const [title, setTitle] = useState('')
   const [author, setAuthor] = useState('')
   const [url, setUrl] = useState('')
+  const [visible, setVisible] = useState(false)
 
   const handleCreate = (e) => {
-    e.preventDefault()
-    props.createBlog({
-      title,
-      author,
-      url
-    })
-    setTitle('')
-    setAuthor('')
-    setUrl('')
+      e.preventDefault()
+      props.createBlog({
+        title,
+        author,
+        url
+      })
+      setTitle('')
+      setAuthor('')
+      setUrl('')
+      setVisible(false)
+  }
+
+  const hideWhenVisible = {
+    display: visible ? 'none' : ''
+  }
+
+  const showWhenVisible = {
+    display: visible ? '' : 'none'
   }
 
   return (
-    <form onSubmit={handleCreate}>
-        <h1>create new</h1>
-        <div><label>title: <input type="text" name="title" value={title} onChange={(e) => setTitle(e.target.value)} /></label></div>
-        <div><label>author: <input type="text" name="author" value={author} onChange={(e) => setAuthor(e.target.value)} /></label></div>
-        <div><label>url: <input type="text" name="url" value={url} onChange={(e) => setUrl(e.target.value)} /></label></div>
-        <button>create</button>
-    </form>
+    <div>
+      <div style={hideWhenVisible}>
+        <button onClick={() => setVisible(true)}>new blog</button>
+      </div>
+      <form style={showWhenVisible} onSubmit={handleCreate}>
+          <h1>create new</h1>
+          <div><label>title: <input type="text" name="title" value={title} onChange={(e) => setTitle(e.target.value)} /></label></div>
+          <div><label>author: <input type="text" name="author" value={author} onChange={(e) => setAuthor(e.target.value)} /></label></div>
+          <div><label>url: <input type="text" name="url" value={url} onChange={(e) => setUrl(e.target.value)} /></label></div>
+          <button>create</button>
+      </form>
+      <button style={showWhenVisible} onClick={() => setVisible(false)}>cancel</button>
+    </div>
   )
 }
