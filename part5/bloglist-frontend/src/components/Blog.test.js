160223476c1f1f8e0d113a8769ad42491cb2bbe4
clemcodes <clemcodes@outlook.com>
2023-06-12 16:08:35 +0100

    5.14 | test - show elements when button clicked

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import Blog from "./Blog";
+import userEvent from "@testing-library/user-event";
 
 describe("<Blog />", () => {
   it("renders blog's title and author initially but not url or number of likes", () => {
@@ -19,5 +20,24 @@ describe("<Blog />", () => {
 
     expect(screen.getByText("test blog test author")).toBeDefined();
     expect(screen.getByTestId("invisible")).toHaveStyle("display:none");
+  });
+
+  it("renders blog's url and likes when the view button is clicked", () => {
+    const blog = {
+      title: "test blog",
+      author: "test author",
+      url: "/test-url",
+      likes: 0,
+      user: {
+        name: "jaycee",
+      },
+    };
+
+    render(<Blog blog={blog} />);
+
+    const button = screen.getByText("view");
+    userEvent.click(button);
+
+    expect(screen.getByTestId("invisible")).toHaveStyle("display:block");
   });
 });
