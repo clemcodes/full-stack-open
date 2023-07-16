c7477ca08c2d77efcf52e7e6f79b7adf74fee3c8
clemcodes <clemcodes@outlook.com>
2023-07-16 13:04:56 +0100

    8.4 | books of an author

--- part8/library-backend.js
+++ part8/library-backend.js
@@ -114,7 +114,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -123,7 +123,13 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (!args.author) {
+        return books;
+      }
+      const byAuthor = (book) => book.author === args.author;
+      return books.filter(byAuthor);
+    },
     allAuthors: () => {
       return authors.map((author) => {
         const bookCount = books.filter(
