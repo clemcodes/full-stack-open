ceb5ce23daa4599f93791b8eff3cecfd28fc9cf4
clemcodes <clemcodes@outlook.com>
2023-07-16 15:31:30 +0100

    8.7 | mutation - update birth year

--- part8/library-backend.js
+++ part8/library-backend.js
@@ -127,6 +127,11 @@ const typeDefs = `
       published: Int!
       genres: [String!]!
     ): Book
+
+    editAuthor(
+      name: String!
+      setBornTo: Int
+    ): Author
   }
 `;
 
@@ -169,6 +174,18 @@ const resolvers = {
       }
 
       return book;
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find((author) => author.name === args.name);
+      if (!author) {
+        return null;
+      }
+
+      const updatedAuthor = { ...author, born: args.setBornTo };
+      authors = authors.map((author) =>
+        author.name === args.name ? updatedAuthor : author
+      );
+      return updatedAuthor;
     },
   },
 };
